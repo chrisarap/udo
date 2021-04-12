605ad82516b22575ffd997b5ac208b3dba6eca39
chrisarap <carteaga460@gmail.com>
2021-04-12 07:37:21 -0400

    configuring mail function

--- contact.php
+++ contact.php
@@ -5,14 +5,19 @@
 		include "functions.php";
 		print_meta("Contacto");
 	?>
-	
+
 </head>
 <body>
 	<?php 
 		print_nav();
 	?>
 
-	<input type="text" name="" placeholder="Asunto del mensaje">
-	<input type="text" name="" placeholder="Mensaje">
+	<form method="post" action=" email_control() ">
+		<input type="text" name="" placeholder="Asunto del mensaje">
+		<textarea placeholder="Escriba su mensaje aquí..."></textarea>
+		<input type="text" name="" placeholder="Nombre y apellido">
+		<input type="text" name="" placeholder="Correo">
+		<input type="submit" name="" value="Enviar">
+	</form>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -42,4 +42,8 @@
 		);
 		return $formats;
 	}
+
+	function email_control(){
+		print "work";
+	}
 ?>
