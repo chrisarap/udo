8aaf6e357b2ae45399ced70e6d11d2fb29c5d780
chrisarap <carteaga460@gmail.com>
2021-04-15 22:22:55 -0400

    contact style fixed

--- contact.php
+++ contact.php
@@ -12,14 +12,17 @@
 		print_nav();
 	?>
 
-	<form method="post"  action="#" >
+
+	<h2>Información de contacto</h2>
+	<form method="post"  action="#" class="contact" >
 		<input type="text" name="subject"	placeholder="Asunto del mensaje">
-		<input type="text" name="pip"		placeholder="PIP">
+		<textarea name="message" placeholder="Escriba su mensaje aquí..."></textarea>
 		<input type="text" name="name"	 	placeholder="Nombre y apellido">
+		<input type="text" name="pip"		placeholder="PIP">		
 		<input type="text" name="email"		placeholder="Correo">
 
 		
-		<textarea name="message" placeholder="Escriba su mensaje aquí..."></textarea>
+		
 
 		<input type="submit" name="btn_contact" value="Enviar">
 	</form>
